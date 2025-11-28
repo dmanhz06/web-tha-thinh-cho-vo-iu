ca92ac5d982efd2596308f6f225025a842178b51
dmanhz <126948734+dmanhz06@users.noreply.github.com>
2025-11-28 15:13:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  titleWeb: "Gửi vợ chang iu dấu của chồng",
+  introTitle: "vợ chang yêu dấu",
+  introDesc: `Vợ như bánh mì bơ sữa,
+    Đặc biệt thơm ngon cho chồng thưởng thức hehe 😘😘`,
+  btnIntro: "^^HiHi^^",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰",
+  desc: "Thoát web là chấp nhận yêu chồng suốt đời rồi nhé!!!",
+  btnYes: "Thích lắm <33",
+  btnNo: "Không nha :3",
+  question:
+    "Trên thế giới hơn 7 tỉ người mà sao vợ chang lại chọn yêu chồng bin dọ <3",
+  btnReply: "Gửi cho bạn <3",
+  reply: "vì vợ yêu chồng bin nhiều nhấttttt <33333333",
+  mess: "chồng biết mà 🥰. Yêu cún vợ nhiều nhiều 😘😘",
+  messDesc: "Noel, chồng về Vũng Tàu đón vợ đi chơi nhá.",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/duy.manh.581448",
+};
